ac27ff112818509db092fe42a2340b64bdc543f2
LuanVasco <devluangodoy@gmail.com>
2021-04-26 22:14:13 -0300

    feat create context playing to listening the audio

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -21,9 +21,11 @@ type Episode = {
   url: string;
   publishedAt: string;
 }
+
 type EpisodeProps = {
   episode: Episode;
 }
+
 export default function Episode({ episode }: EpisodeProps) {
   return (
     <article className={styles.episode}>
@@ -60,8 +62,24 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc',
+    }
+  })
+
+  const paths = data.map(episode => {
+    return {
+      params: {
+        slug: episode.id
+      }
+    }
+  })
+
   return {
-    paths: [],
+    paths,
     fallback: 'blocking'
   }
 }
